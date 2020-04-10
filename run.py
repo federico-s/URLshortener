82f01d26b139a48f270eae732210a4b4fb0ad7c5
federico.scardina <federico.scardina@soft.it>
2020-04-10 12:36:09 +0200

    wip

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from flask import Flask
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
